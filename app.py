3ba796153779389736bacd5183be07a4c26c6000
ravigoel08 <ravigoel.1997@gmail.com>
2019-10-19 19:50:54 +0530

    requirement1

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ def pigro():
 	if(msg.lower().strip()!="hi" and msg.lower().strip()!="hello" and msg.lower().strip()!="help" and msg.lower().strip()!="about" and msg.lower().strip()!="pigro" and msg.lower().strip()!="pigrobot"):
 		try:
 			req = requests.get(f'https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=1&q={msg}').json()
-			heading = req['Heading']
-			ab_text = req['AbstractText']
+			#heading = req['Heading']
+			#ab_text = req['AbstractText']
 			image = req['Image']
 			if ab_text == "":
 				ab_text = req['RelatedTopics'][0]['Text']
 			mes = heading+'\n\n'+ab_text
 			resp = MessagingResponse()
-			resp.message(f"{mes}")
+			#resp.message(f"{mes}")
+			resp.message(`*`+req['Heading']+`*`+'\n\n'+ req['AbstractText'] + image)
 			return str(resp)
 		except:
 			resp = MessagingResponse()
@@ -30,7 +31,7 @@ def pigro():
 			return str(resp)
 	else:
 		resp = MessagingResponse()
-		resp.message('I am *PigroBot* which help you to get quick information from WikiPedia pages, right within WhatsApp.\nTry me, send me anything you want information about. \n I am Developed by *Ravi Goyal*. Follow him 💚  https://www.instagram.com/sypherrv/ ')
+		resp.message('Hey 👋 I am *PigroBot* which help you to get quick information from WikiPedia pages, right within WhatsApp.\nTry me, send me anything you want information about. \n I am Developed by *Ravi Goyal*. Follow him 💚  https://www.instagram.com/sypherrv/ ')
 		return str(resp)
 if __name__=='__main__':
 	app.run(threaded=True, port=5000)
