302cd87c9d470bb60edc84651ee490ee6b7d6559
ravigoel08 <ravigoel.1997@gmail.com>
2019-10-19 21:42:23 +0530

    done

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def server():
 @app.route('/incoming',methods=['POST'])
 def pigro():
 	msg = request.form.get('Body')
-	print(msg)
+	resp = MessagingResponse()
 	if(msg.lower().strip()!="hi" and msg.lower().strip()!="hello" and msg.lower().strip()!="help" and msg.lower().strip()!="about" and msg.lower().strip()!="pigro" and msg.lower().strip()!="pigrobot"):
 		try:
 			req = requests.get(f'https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=1&q={msg}').json()
@@ -20,15 +20,12 @@ def pigro():
 			if ab_text == "":
 				ab_text = req['RelatedTopics'][0]['Text']
 			#mes = heading+'\n\n'+ab_text
-			resp = MessagingResponse()
-			resp.message(`*`+req["Heading"]+`*`+req["ab_text"])
+			msg = resp.message(`*`+req["Heading"]+`*`+req["ab_text"])
 			return str(resp)
 		except:
-			resp = MessagingResponse()
 			resp.message('_Result not found!_')
 			return str(resp)
 	else:
-		resp = MessagingResponse()
 		resp.message('Hey 👋 I am *PigroBot* which help you to get quick information from WikiPedia pages, right within WhatsApp.\nTry me, send me anything you want information about. \n I am Developed by *Ravi Goyal*. Follow him 💚  https://www.instagram.com/sypherrv/ and https://github.com/ravigoel08/')
 		return str(resp)
 if __name__=='__main__':
