3f0aea673c59de9df485f3b8537a164ac93c4465
ravigoel08 <ravigoel.1997@gmail.com>
2020-12-19 18:14:00 +0530

    Update project structure

---
+++ src/searcher.py
@@ -0,0 +1,12 @@
+def Check(msg: str)-> bool:
+    msg = msg.lower().strip()
+    return True if (msg!="hi" and msg!="hello" and msg!="help" and msg!="about" and msg!="pigro" and msg!="pigrobot") else False
+
+def Searcher(msg: str)->
+    payload = {'skip_disambig':'1', 'format': 'json', 'pretty': '1', 'q': msg}
+    req = requests.get('https://api.duckduckgo.com/', params=payload).json()
+    ab_text = req['AbstractText']
+    if ab_text == "":
+        ab_text = req['RelatedTopics'][0]['Text']     
+    res = '*'+req["Heading"]+'*'+'\n\n'+ab_text
+    return res   
---
+++ src/views.py
@@ -0,0 +1,26 @@
+from src import app
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import request, jsonify
+import requests
+import constants
+from searcher import Check, Searcher
+
+@app.route('/server',methods=['GET'])
+def server():
+	return 'hello'
+
+@app.route('/incoming',methods=['POST'])
+def pigro():
+	msg = request.form.get('Body')
+	resp = MessagingResponse()
+	if(Check(msg)):
+		try:
+			res = Searcher(msg)
+			msg = resp.message(res)
+			return str(resp)
+		except:
+			resp.message(constants.RESULT)
+			return str(resp)
+	else:
+		resp.message(constants.MESSAGE)
+		return str(resp)
