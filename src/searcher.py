fa3f2e717a27f00d694ab52d1332a1d1c816f96b
Ravi Goyal <31596295+ravigoel08@users.noreply.github.com>
2020-12-19 18:17:03 +0530

    Update searcher.py

--- src/searcher.py
+++ src/searcher.py
@@ -1,8 +1,10 @@
+import requests
+
 def Check(msg: str)-> bool:
     msg = msg.lower().strip()
     return True if (msg!="hi" and msg!="hello" and msg!="help" and msg!="about" and msg!="pigro" and msg!="pigrobot") else False
 
-def Searcher(msg: str)->
+def Searcher(msg: str)-> str:
     payload = {'skip_disambig':'1', 'format': 'json', 'pretty': '1', 'q': msg}
     req = requests.get('https://api.duckduckgo.com/', params=payload).json()
     ab_text = req['AbstractText']
